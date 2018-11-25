222ff16c150d3e9e8ce429b92aa66facb05f3496
Suchin Gururangan <github@suchin.io>
2018-11-24 23:30:51 -0800

    first change

--- allennlp/commands/train.py
+++ allennlp/commands/train.py
@@ -146,7 +146,7 @@ def datasets_from_params(params: Params) -> Dict[str, Iterable[Instance]]:
     """
     Load all the datasets specified by the config.
     """
-    dataset_reader = DatasetReader.from_params(type="train", params.pop('dataset_reader'))
+    dataset_reader = DatasetReader.from_params(params.pop('dataset_reader'), type="train")
     validation_dataset_reader_params = params.pop("validation_dataset_reader", None)
 
     validation_and_test_dataset_reader: DatasetReader = dataset_reader
